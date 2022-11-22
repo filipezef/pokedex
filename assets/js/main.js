9ebefd9d008944b02ead63bd690c2046905b0fcb
Filipe Zeferino <filipezef@gmail.com>
2022-11-22 08:05:54 -0300

    pagination added, along with limit for pokemon items and message when limit is reached

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,68 @@
-function convertPokemonToLi(pokemon) {
-  return `
-  <li class="pokemon ${pokemon.type}">
-    <span class="number">#${
-      (pokemon.number < 10 ? '00' : '0') + String(pokemon.number)
-    }</span>
-    <span class="name">${pokemon.name}</span>
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+let offset = 0
+const limit = 5
 
-    <div class="detail">
-      <ol class="types">
-        ${pokemon.types
-          .map(type => `<li class="type ${pokemon.type}">${type}</li>`)
-          .join('')}
-      </ol>
-      <img
-        class="pokemonImage"
-        src="${pokemon.image}"
-        alt="${pokemon.name}"
-      />
-    </div>
-  </li>
-  `
+// to adjust pokemon order number to 3 digits
+function adjustPokemonOrder(order) {
+  if (order < 10) {
+    order = '00' + String(order)
+  } else if (order > 9 && order < 100) {
+    order = '0' + String(order)
+  }
+  return order
 }
 
-const pokemonList = document.getElementById('pokemonList')
+function loadMorePokemons(offset, limit) {
+  pokeApi
+    .getPokemons(offset, limit)
+    .then((pokemons = []) => {
+      // DIO method, based on a new const newHtml
+      // const newHtml = pokemons.map(convertPokemonToLi).join('')
+      // pokemonList.innerHTML = newHtml
+      pokemonList.innerHTML += pokemons
+        .map(
+          pokemon => `
+          <li class="pokemon ${pokemon.type}">
+            <span class="number">#${adjustPokemonOrder(pokemon.number)}</span>
+            <span class="name">${pokemon.name}</span>
+        
+            <div class="detail">
+              <ol class="types">
+                ${pokemon.types
+                  .map(type => `<li class="type ${pokemon.type}">${type}</li>`)
+                  .join('')}
+              </ol>
+              <img
+                class="pokemonImage"
+                src="${pokemon.image}"
+                alt="${pokemon.name}"
+              />
+            </div>
+          </li>
+          `
+        )
+        .join('')
+    })
+    .catch(err => console.error(err))
+}
 
-pokeApi
-  .getPokemons()
-  .then(pokemons => {
-    pokemonList.innerHTML += pokemons
-      .map(pokemon => convertPokemonToLi(pokemon))
-      .join('')
-  })
-  .catch(err => console.error(err))
+loadMorePokemons(offset, limit)
+
+const div = document.createElement('div')
+div.innerText = 'offset is out of range'
+const body = document.querySelector('body')
+const divBtn = document.getElementById('pagination')
+
+loadMoreButton.addEventListener('click', () => {
+  offset += limit
+  console.log(offset)
+  // to limit offset to the number of 1st generation pokemons (151)
+  if (offset < 15) {
+    loadMorePokemons(offset, limit)
+  } else {
+    console.log('offset is out of range')
+    divBtn.insertBefore(div, loadMoreButton)
+    loadMoreButton.remove()
+  }
+})
